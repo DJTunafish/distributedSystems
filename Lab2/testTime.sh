e79ae2b5005e701e5c06b1a3adf76b507d9f55ae
Patrick Franz <patfra71@gmail.com>
2017-11-30 10:15:45 +0100

    Changed methodology of test-script

--- Lab2/testTime.sh
+++ Lab2/testTime.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-	echo "Need 1 argument - number of servers"
+	echo "Need 2 arguments - 1. number of servers 2. number of requests per server"
 	exit 1
 fi
 
 no_servers=$1
-for i in `seq 1 ${no_servers}`; do
-	SERVER=$((i))
-	IPPREFIX="10.1.0."
-	IPSUFFIX="/entries"
-	IP=${IPPREFIX}${SERVER}${IPSUFFIX}
-	echo "Sending requests to server ${SERVER} ..."
-	for j in `seq 1 40`; do
-			curl -s --data 'entry=test'${i} -X POST ${IP}
+no_requests=$2
+echo "Sending requests ..."
+for i in `seq 1 ${no_requests}`; do
+	echo "Sending request no. ${i}"
+	for j in `seq 1 ${no_servers}`; do
+		SERVER=$((j))
+		IPPREFIX="10.1.0."
+		IPSUFFIX="/entries"
+		IP=${IPPREFIX}${SERVER}${IPSUFFIX}
+		curl -s --data 'entry=test'${i} -X POST ${IP}
 	done
 done
+echo "Done."
